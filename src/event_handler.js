ceb11e883a5bc75991db8e3f10af0dbed0776a30
viktoriabloch <e247769@sbl.ch>
2023-03-10 15:20:11 +0100

    bäume auf den seiten als absperrung

--- src/event_handler.js
+++ src/event_handler.js
@@ -6,7 +6,7 @@ export default class EventHandler {
   constructor() {
     this.events = new Set()
     // Setup Eventlisteners
-    window.onkeydown = (ev) => {this.events.add(ev.code)}
+    window.onkeydown = (ev) => {this.events.add(ev.code); return false}
     window.onkeyup = (ev) => {this.events.delete(ev.code)}
   }
 
