1621f7f71920e201641351d80ce9aeb49369d96c
viktoriabloch <e247769@sbl.ch>
2023-03-17 09:14:35 +0100

    grösse des spielfelds verändert

--- src/game.js
+++ src/game.js
@@ -17,8 +17,8 @@ export default class Game {
   constructor() {
     this.tileSize = 32
     this.canvas = document.querySelector("#canvas")
-    this.canvas.width = 10 * this.tileSize
-    this.canvas.height = 15 * this.tileSize
+    this.canvas.width = 40 * this.tileSize
+    this.canvas.height = 10 * this.tileSize
     this.ctx = this.canvas.getContext("2d")
     this.ctx.imageSmoothingEnabled = false
 
